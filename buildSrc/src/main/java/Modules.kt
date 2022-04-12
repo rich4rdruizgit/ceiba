0625d94781a85a354f7f533affe5de561087b909
rich4rdruizgit <rich4rdruiz@gmail.com>
2022-04-11 20:49:57 -0500

    (gradle): configurando dependencias

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,3 +1,11 @@
 object Modules {
     const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val users = ":users"
+    const val usersDomain = ":users:users_domain"
+    const val usersData = ":users:users_data"
+    const val usersPresentation = ":users:users_presentation"
 }
